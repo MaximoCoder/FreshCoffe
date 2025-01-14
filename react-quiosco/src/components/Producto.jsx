cc893cdc775ad084f8ca4ceb6d8267c1200c0b0b
Max <maximonaci@gmail.com>
2025-01-13 21:05:39 -0600

    Modal de añadir producto

--- react-quiosco/src/components/Producto.jsx
+++ react-quiosco/src/components/Producto.jsx
@@ -1,6 +1,10 @@
 // Importamos helpers
 import { formatearDinero } from '../helpers'
+// Importamos el hook
+import useQuiosco from '../hooks/useQuiosco'
 export default function Producto({producto}) {
+    // Para el modal
+    const {handleClickModal, handleSetProducto} = useQuiosco()
     // Destructuring
     const {imagen, nombre, precio} = producto
   return (
@@ -17,6 +21,10 @@ export default function Producto({producto}) {
             <button 
                 type="button"
                 className="bg-indigo-600 hover:bg-indigo-700 w-full mt-5 p-3 text-white font-bold uppercase "
+                onClick={() => {
+                    handleClickModal();
+                    handleSetProducto(producto);
+                }}
             >
                 Agregar
             </button>
--- react-quiosco/src/context/QuioscoProvider.jsx
+++ react-quiosco/src/context/QuioscoProvider.jsx
@@ -9,19 +9,37 @@ const QuioscoProvider = ({ children }) => {
     const [categorias, setCategorias] = useState(categoriasDB);
     // Categoria actual
     const [categoriaActual, setCategoriaActual] = useState(categorias[0])
-
+    // Modal
+    const [modal, setModal] = useState(false)
+    // Productos
+    const [producto, setProducto] = useState({})
+    // Funcion para cuando se cambia la categoria
     const handleClickCategoria = id =>{
         // Filtrar categorias por id 
         const categoria = categorias.filter(categoria => categoria.id === id)[0]
         // Definir la categoria actual 
         setCategoriaActual(categoria)
     }
+
+    // Funcion para el modal
+    const handleClickModal = () => {
+        setModal(!modal)
+    }
+
+    // Funcion para los productos
+    const handleSetProducto = producto => {
+        setProducto(producto)
+    }
     return (
         <QuioscoContext.Provider
             value={{
                 categorias,
                 categoriaActual,
-                handleClickCategoria
+                handleClickCategoria, 
+                modal, 
+                handleClickModal,
+                producto,
+                handleSetProducto
             }}
         >
             {children}
